67d3999cd2543d6099d6e32c402cb5254fcbe931
Tony Hillerson <tony.hillerson@gmail.com>
2010-12-22 09:57:32 -0700

    Fixed an error where newly added book links had an undefined name

--- public/index.js
+++ public/index.js
@@ -5,13 +5,13 @@ socket.on('message', function(data){
   var messageType = message['type'];
   switch(messageType) {
   case "book-created" :
-    var book = message['book'];
+    var book = JSON.parse(message['book']);
     var bookId = book['_id'];
     var itemId = "li#" + bookId;
     
     $("#task-books-list").append("<li id='" + bookId + "'><a href='/books/" + bookId + "'>" + book['name'] + "</a></li>");
     $(itemId).effect(
       "highlight", {}, 3000
-    ); 
-  }
+    );
+  };
 });
