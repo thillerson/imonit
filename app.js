a7fe1ac35ce2b78ef405a5199c1ba7507828c044
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-12-23 14:26:08 -0700

    fixed logging

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.get('/', function(req, res) {
 });
 
 app.post('/books', function(req, res) {
-  console.log("body: " + body);
+  console.log("body: " + req.body);
   var bookParams = req.body.book;
   var taskBook = new TaskBook();
   taskBook.name = bookParams['name'];
